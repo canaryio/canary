4d9cce31e73a6bf831c506187bd4f9dd32e61c99
John Meichle <john.meichle@acquia.com>
2015-02-18 00:18:55 -0500

    Update manifest_test to test the interval definiton on urls

--- manifest_test.go
+++ manifest_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 )
 
-func TestGetManifest(t *testing.T) {
+func TestGetManifestWithoutInterval(t *testing.T) {
 	handler := func(w http.ResponseWriter, r *http.Request) {
 		data := `{
 			"targets": [
@@ -40,5 +40,50 @@ func TestGetManifest(t *testing.T) {
 
 	if target.URL != "http://www.canary.io" {
 		t.Fatal("expected URL to be equal to 'http://www.canary.io', got %s", target.URL)
+	}
+
+	if target.Interval != 0 {
+		t.Fatal("expected Interval to be equal to zero when undefined in the manifest json, got %d", target.Interval)
+	}
+}
+
+func TestGetManifestWithInterval(t *testing.T) {
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		data := `{
+			"targets": [
+				{
+					"url": "http://www.canary.io",
+					"name": "canary",
+					"interval": 2
+				},
+				{
+					"url": "http://www.github.com",
+					"name": "github",
+					"interval": 4
+				}
+			]
+		}`
+
+		fmt.Fprintf(w, data)
+	}
+
+	ts := httptest.NewServer(http.HandlerFunc(handler))
+	defer ts.Close()
+
+	m, err := GetManifest(ts.URL)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	first_target := m.Targets[0]
+
+	if first_target.Interval != 2 {
+		t.Fatal("expected Interval on the first target to be equal to the manifest json definition of 2, got %d", first_target.Interval)
+	}
+
+	second_target := m.Targets[1]
+
+	if second_target.Interval != 4 {
+		t.Fatal("expected Interval on the second target to be equal to the manifest json definition of 4, got %d", second_target.Interval)
 	}
 }
