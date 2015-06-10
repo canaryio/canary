e5153b971843604018e91387534729c2a3d37660
Michael Gorsuch <michael.gorsuch@gmail.com>
2015-06-10 00:29:15 +0000

    WIP: preallocate our buffer so we are more efficient

--- pkg/httppublisher/publisher.go
+++ pkg/httppublisher/publisher.go
@@ -3,6 +3,7 @@ package httppublisher
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"time"
@@ -10,19 +11,23 @@ import (
 	"github.com/canaryio/canary/pkg/sensor"
 )
 
+const MAX_BUFFER = 1000
+
 type Publisher struct {
 	url      string
 	interval time.Duration
 	buffer   []*sensor.Measurement
 	c        chan *sensor.Measurement
+	i        int
 }
 
 func New(url string, interval time.Duration) *Publisher {
 	p := &Publisher{
-		buffer:   make([]*sensor.Measurement, 0),
+		buffer:   make([]*sensor.Measurement, MAX_BUFFER),
 		c:        make(chan *sensor.Measurement),
 		url:      url,
 		interval: interval,
+		i:        0,
 	}
 	go p.run()
 	return p
@@ -35,26 +40,42 @@ func (p *Publisher) Publish(m sensor.Measurement) error {
 
 func (p *Publisher) run() {
 	t := time.NewTicker(p.interval)
+
 	for {
 		select {
 		case <-t.C:
-			payload, err := json.Marshal(p.buffer)
+			// flush when our ticker fires
+			err := p.flush()
 			if err != nil {
-				log.Printf("httppublisher err='%s'", err)
+				log.Print(err)
 			}
-
-			resp, err := http.Post(p.url, "application/json", bytes.NewBuffer(payload))
-			if err != nil {
-				log.Printf("httppublisher err='%s'", err)
-			}
-
-			if resp.StatusCode != 200 || resp.StatusCode != 201 || resp.StatusCode != 202 {
-				log.Printf("httppublisher err='http status' status_code=%d", resp.StatusCode)
-			}
-
-			p.buffer = make([]*sensor.Measurement, 0)
 		case m := <-p.c:
-			p.buffer = append(p.buffer, m)
+			p.buffer[p.i] = m
+			p.i++
+
+			// flush if we've exceeded the bounds of our buffer
+			if p.i >= MAX_BUFFER {
+				p.flush()
+			}
 		}
 	}
+}
+
+func (p *Publisher) flush() error {
+	payload, err := json.Marshal(p.buffer)
+	if err != nil {
+		return fmt.Errorf("httppublisher err='%s'", err)
+	}
+
+	resp, err := http.Post(p.url, "application/json", bytes.NewBuffer(payload))
+	if err != nil {
+		return fmt.Errorf("httppublisher err='%s'", err)
+	}
+
+	if resp.StatusCode != 200 || resp.StatusCode != 201 || resp.StatusCode != 202 {
+		return fmt.Errorf("httppublisher err='http status' status_code=%d", resp.StatusCode)
+	}
+
+	p.i = 0
+	return nil
 }
